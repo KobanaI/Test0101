c0f365c630e3726a17fddfbfaf27b0955a05773b
koba <vrnamiki@gmail.com>
2024-12-29 00:55:10 +0900

    Initial commit

---
+++ my-app/my-lib/main.py
@@ -0,0 +1,53 @@
+import sys,secrets,string,requests,cv2,os
+from pathlib import Path
+
+server_url = os.getenv("SERVER_URL", "http://localhost:4000")
+uploaded_file_path = sys.argv[1]
+
+
+def deleteReq():
+  
+  path = Path(uploaded_file_path)
+  try:
+    path.unlink()
+    print(f"{path} は跡形もなく消え去りました")
+  except :
+    print("なんかエラーでたぞpython")
+
+def generate_random_string(length=10):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    random_string = ''.join(secrets.choice(alphabet) for _ in range(length))
+    return random_string
+
+
+# 画像を読み込む
+if uploaded_file_path :
+    print(1122)
+    image = cv2.imread(uploaded_file_path)
+    image_change1 = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    random = "dsdasdsaasfaddasdsf"
+
+    app_dir = Path(__file__).resolve().parent.parent  # my-app フォルダ
+    save_dir = app_dir / 'complete_images'  # my-app/complete_images フォルダ
+    save_dir.mkdir(parents=True, exist_ok=True)
+    
+    save_path = save_dir / f"{random}.png"
+    print('保存パスはこれです', save_path)
+    deleteReq()
+    cv2.imwrite(str(save_path), image_change1)  # 変換した画像を保存
+
+
+    data = {"path": str(save_path)}  # 辞書形式に変更
+    requests.post(f'{server_url}/composition/completeChangeImg',json=data)
+
+
+else:
+  print('そもそも画像がないよ')
+
+
+
+
+
+
+
+
---
+++ my-app/routes/composition.js
@@ -0,0 +1,157 @@
+const express = require("express");
+const path = require("path");
+const formidable =require('formidable')
+const app = express();
+const fs = require('fs')
+const { exec } = require('child_process');
+
+
+///composition/
+app.get("/", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Upload a Photoえくす</title>
+    </head>
+    <body>
+        <h1>Upload a Photo EXpressdsafdsa</h1>
+        <form id="form1" enctype="multipart/form-data" action= "/composition/process-file" method="POST">
+            <input type="file" name="file" id="fileInput" accept="image/*" required>
+            <button type="submit">Upload</button>
+        </form>
+
+        <img src="https://k-net01.com/wp-content/uploads/2019/08/get-image-url-03.jpg" alt="画像">
+
+<script>
+
+document.getElementById("form1").addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+
+  try {
+    const response = await fetch("/composition/process-file", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log("アップロード成功:", data);
+
+      // アップロードされたファイルの表示ページにリダイレクト
+      window.location.href = "/composition/process-file";
+    } else {
+      console.error("アップロードに失敗しました");
+      alert("アップロードに失敗しました");
+    }
+  } catch (error) {
+    console.error("リクエストエラー:", error);
+    alert("リクエスト中にエラーが発生しました");
+  }
+});
+
+</script>
+
+    </body>
+    </html>
+  `);
+});
+
+let uploadedFilePath = "";
+
+///composition/process-file
+app.post("/process-file", (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.uploadDir = path.join(__dirname, '../uploads'); // 保存ディレクトリ
+  form.keepExtensions = true; // 拡張子を保持
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.error("ファイルアップロード中のエラー:", err);
+      res.status(500).send("ファイルのアップロード中にエラーが発生しました");
+      return;
+    }
+
+    
+    if (files.file) {
+      uploadedFilePath = files.file[0].filepath; // ファイルパスを格納
+      sendToPython(res);
+    } else {
+      res.status(400).send("ファイルがアップロードされていません");
+    }
+  });
+
+});
+
+
+function sendToPython(res){
+  
+  const pythonScriptPath = path.resolve(__dirname, '../my-lib/main.py');
+  const pythonCommand = `python "${pythonScriptPath}" "${uploadedFilePath}"`;
+  exec(pythonCommand, (error, stdout, stderr) => {
+    if (error || stderr) {
+      console.error("Pythonエラー:", error? error:stderr);
+    }
+    else{
+      res.json({ message: "処理成功!!!", pythonOutput: stdout });
+    }
+
+  });
+}
+
+//ここめちゃくちゃ苦労した
+let fileUrl = "";
+let absolutePath = "";
+app.post("/completeChangeImg", (req,res)=>{
+  const resolvedPath = path.resolve(req.body.path);
+  absolutePath = resolvedPath;
+  const basePath = path.join(__dirname,'./'); //ここよくわかっていない
+  const relativePath = path.relative(basePath, resolvedPath);
+  const finalPath = relativePath.replace(/\\/g, '/');
+  console.log('変換された相対パス:', finalPath);
+  fileUrl = relativePath.replace(/\\/g, '/');
+  res.redirect('/process-file')
+})
+
+///composition/process-file
+app.get("/process-file", (req, res) => {
+  if (!fileUrl) {
+    res.status(400).send("画像のデータが消失したため、もう一度やり直してください");
+    return;
+  }
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Upload Result</title>
+    </head>
+    <body>
+        <h1>Uploaded File</h1>
+        <img src="${fileUrl}" alt="Uploaded Image" style="max-width: 100%; height: auto;">
+        <p>ファイルパス: ${fileUrl}</p>
+    </body>
+    </html>
+  `);
+
+  const deleteDelay = 1000; // 1秒後に削除
+  setTimeout(() => {
+    fs.unlink(absolutePath, (err) => {
+      if (err) {
+        console.error('ファイルの削除に失敗しました:', err);
+      } else {
+        console.log('ファイルが削除されました:', absolutePath);
+      }
+    });
+  }, deleteDelay);
+
+
+});
+
+module.exports = app;
+
+
