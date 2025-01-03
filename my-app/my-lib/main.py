be8a9d7cf07c53cb33e7c1599bf1ce8e3da0408b
koba <vrnamiki@gmail.com>
2025-01-03 09:47:57 +0900

    Initial commit

--- my-app/my-lib/main.py
+++ my-app/my-lib/main.py
@@ -6,8 +6,8 @@ from pathlib import Path
 
 
 #SERVER_URL認識されない。
-#server_url = os.getenv("SERVER_URL")
-server_url = os.getenv("http://localhost:4000","http://localhost:4000")
+cdserver_url = os.getenv("SERVER_URL")
+#server_url = os.getenv("http://localhost:4000","http://localhost:4000")
 uploaded_file_path = sys.argv[1]
 
 def deleteOriginalFile():
